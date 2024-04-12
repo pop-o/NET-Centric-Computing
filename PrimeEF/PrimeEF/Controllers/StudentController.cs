6d0057d85555739fae7c061ce7aa53f6c2979dcc
Poem Maharjan <poemmahrajan4290@gmail.com>
2024-04-12 12:57:14 +0545

    Edit_pt_2

--- PrimeEF/PrimeEF/Controllers/StudentController.cs
+++ PrimeEF/PrimeEF/Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PrimeEF.Models;
+using System.Diagnostics.Eventing.Reader;
 //controller is responsible for passing data to model and view
 namespace PrimeEF.Controllers
 {
@@ -43,5 +44,55 @@ namespace PrimeEF.Controllers
 
             return View(viewStd);
         }
+        //action method for update operation
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            //check if id sent from view is matched with database record or not
+            //if matched send the data to set method od EditStudent
+            var editData=sc.Students.FirstOrDefault(x => x.Id == id);
+            if(editData != null)
+            {
+                //if id mathced with db table record
+                var editStd = new EditStudent()
+                {
+                    Id=editData.Id,
+                    Name=editData.Name,
+                    Gender=editData.Gender,
+                    Address=editData.Address,
+                    Faculty=editData.Faculty
+                };
+                return View(editStd);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            
+           
+
+        }
+        [HttpPost]
+        public IActionResult Edit(EditStudent es) 
+        {
+            //checking if id of editStudent persit or not in database
+            //save changes in database
+            var editStds = sc.Students.Find(es.Id);
+            if(editStds != null )
+            {
+                editStds.Id = es.Id;
+                editStds.Name = es.Name;
+                editStds.Gender = es.Gender;
+                editStds.Address = es.Address;  
+                editStds.Faculty = es.Faculty;
+                sc.Students.Add(editStds);
+                sc.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+        
+
     }
+    
 }
---
+++ PrimeEF/PrimeEF/Models/EditStudent.cs
@@ -0,0 +1,14 @@
+﻿namespace PrimeEF.Models
+{
+    //this model is responsible for setting the data for update
+    public class EditStudent
+    {
+        public Guid Id { get; set; }//auto id generation with primary key
+
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+
+        public string Faculty { get; set; }
+    }
+}
