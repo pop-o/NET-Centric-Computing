18e50008683b3812e52fc885a8957ee4807cd8af
Poem Maharjan <poemmahrajan4290@gmail.com>
2024-04-18 12:46:40 +0545

    Asssigment Code

--- StateManagement/StateManagement/Controllers/AuthController.cs
+++ StateManagement/StateManagement/Controllers/AuthController.cs
@@ -13,10 +13,30 @@ namespace StateManagement.Controllers
         {
             return View();
         }
-        [HttpPost]
-        public ActionResult Index(string  )
+        public ActionResult Log()
         {
-            return View();
+            Session.Remove("user");
+            return View("Index");
+        }
+        [HttpPost]
+        public ActionResult Index(string userTxt,string passTxt)
+        {
+            if(userTxt == "Poem" && passTxt == "12345")
+            {
+                Session["user"] = "Poem";
+                TempData["user"] = "Poem";
+                /*HttpCookie kt1 = new HttpCookie("user", "Poem");//non persistent cookie
+                kt1.Expires = DateTime.Now.AddSeconds(30);//to make persistent cookie add expires 
+                Response.Cookies.Add(kt1);
+
+                Response.Cookies["last"].Value="Popo";*/
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                return View();
+            }
+            
         }
     }
 }
